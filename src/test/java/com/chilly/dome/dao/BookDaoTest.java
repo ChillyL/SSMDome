661a3af0e0b301f1a22db9574ed959f5d42c5799
13267 <1550276738@qq.com>
2019-07-25 22:11:22 +0800

    test测试

---
+++ src/test/java/com/chilly/dome/dao/BookDaoTest.java
@@ -0,0 +1,28 @@
+package com.chilly.dome.dao;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//配置spring和junit整合，这样junit在启动时就会加载spring容器
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class BookDaoTest {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    public void addBookByNameIntro() {
+
+    }
+
+    @Test
+    public void findBookByName() {
+
+    }
+}
